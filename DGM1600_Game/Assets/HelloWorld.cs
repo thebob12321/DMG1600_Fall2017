7dd6ddee096c5ecaa8355699211f3d3f85d0c85c
Unknown <darrensmith12@live.com>
2017-08-28 20:14:29 -0600

    The Game

---
+++ DGM1600_Game/Assets/HelloWorld.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HelloWorld : MonoBehaviour {
+
+	string message = "Help Me!";
+
+	// Use this for initialization
+	void Start () {
+		print("Hello World!");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		print(message);
+	}
+}

87dddca97469c84485aefe56d4ab572fdda663d8
Unknown <darrensmith12@live.com>
2017-09-11 20:12:27 -0600

    If and Else

---
+++ conditional.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class conditional : MonoBehaviour {
+	public string stopLight = "Red";
+
+	void start(){
+		Main();
+	}
+	void Main(){
+		if(stopLight == "Red"){
+// this is to test inputs
+		print("STOP!");
+		}
+		else if(stopLight == "Yellow"){
+// you can have as many else if as you want
+			print("Slow Down");
+		}
+		else if(stopLight == "Green"){
+			print("Continue Through");
+		}
+		else{
+//only one else and one if
+// this can be used to bug test any other unplaned out comes
+			print("You're Broken");
+		}
+	}
+}

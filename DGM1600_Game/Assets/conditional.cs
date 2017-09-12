8df4c7d21dacfdf4623058853faa6a03c48cb47a
Unknown <darrensmith12@live.com>
2017-09-11 20:06:16 -0600

    Conditional

---
+++ DGM1600_Game/Assets/conditional.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class conditional : MonoBehaviour {
+	public string stopLight = "Red";
+
+	void start(){
+		Main();
+	}
+	void Main(){
+		if(stopLight == "Red"){
+		print("STOP! Light is red!");
+		}
+		else if(stopLight == "Yellow"){
+			print("Slow Down");
+		}
+		else if(stopLight == "Green"){
+			print("Continue Through");
+		}
+		else{
+			print("You're Broken");
+		}
+	}
+}

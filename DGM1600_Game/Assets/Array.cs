7eb526b1ae086e558a25691b82d533b8e3092820
Unknown <darrensmith12@live.com>
2017-09-20 20:13:40 -0600

    Auto stash before merge of "master" and "origin/master"
    Unity Environment

---
+++ DGM1600_Game/Assets/Array.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// public GameObject[] inventory = new GameObject [12];
+// // 12 is 12, 0-11 total items
+// inventory [0] = "Bazooka";
+// inventory [11] = "Fist";
+
+
+public class Array : MonoBehaviour {
+	public string[] heros = new string[9];
+
+	// Use this for initialization
+	void Start () {
+		heros[0] = "Ironman";
+		heros[1] = "BlackPanther";
+		heros[2] = "Thor";
+		heros[3] = "Hulk";
+		heros[4] = "CaptainAmarica";
+		heros[5] = "Hawkeye";
+
+		print(heros[2]);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}

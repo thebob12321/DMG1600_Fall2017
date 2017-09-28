bf932ec1c1cf1d0e24cadec1a069fec06e263fc4
Unknown <darrensmith12@live.com>
2017-09-27 20:14:34 -0600

    LogicalOp

---
+++ LogicalOp.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LogicalOp : MonoBehaviour {
+
+	public string luke = "Jedi";
+
+	public string vader = "Sith";
+
+	public bool leia = true;
+
+	public string darkSide = "Darth Maul";
+
+	public string lightSide = "Obi Wan";
+
+    public int num1 = 10;
+
+    public float num2 = 10.5f;
+
+	// Use this for initialization
+	void Start () {
+
+        if(num1 == 10 && num2 == 10.5){
+            print("Yes");
+        }
+        else if( num1 == 10 && num2 == 10){
+            print("No");
+        }
+        else {
+            print("Try again");
+        }
+
+	// 	if(luke == "Jedi" && vader == "Nanny"){
+	// 		print("Vaders yo Nanny!");
+	// 	}
+	// 	else if (luke == "Jawa" && vader == "Sith"){
+	// 		print ("Utini!");
+	// 	}		
+	// 	else if (luke == "Jedi" && vader == "Sith"){
+	// 		print("I am your father!");
+	// 	}
+	// 	else {
+	// 		print("You shouldn't see this");
+	// 	}
+
+	// if(leia){
+	// 	print("No, there is another.");
+	// }
+	// else if(!leia){
+	// 	print("Hurry!");
+	// }
+	// else{
+	// 	print("Han fired first");
+	// }
+
+	if(lightSide == "Obi Wan" || darkSide == "Jar Jar Binks"){
+		print("Meeeza Evil Sith Lorda");
+	}
+	else if (lightSide == "Boba Fett" || darkSide == "Darth Maul"){
+		print("As you wish, my master");
+	}
+	else {
+		print("Not the younglings!");
+	}
+
+
+}
+
+	
+
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}

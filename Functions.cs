07bae3dd6f6a6b66bae2625af15253903e240430
Unknown <darrensmith12@live.com>
2017-10-09 19:44:29 -0600

    Functions

---
+++ Functions.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Functions : MonoBehaviour {
+
+	public void  DoStuff(){
+		print("I'm doing stuff...");
+	}
+
+	void Start(){
+		DoStuff();
+		int result = AddNumbers(5,11);
+		print(result);
+	}
+
+	public int AddNumbers(int num1, int num2){
+						//paramaters		
+		int result = num1 + num2;
+		// return makes it accessable
+		return result;
+	}
+
+	
+
+
+}

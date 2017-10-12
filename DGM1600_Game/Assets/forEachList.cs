b322dd4a2ebbaf36574fdf1614172506455d71e3
Unknown <darrensmith12@live.com>
2017-10-11 19:46:06 -0600

    Solo learn

---
+++ DGM1600_Game/Assets/forEachList.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class forEachList : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		List<FirstOrder> badguys = new List<FirstOrder>();
+
+		badguys.Add(new FirstOrder("Snoke",1000));
+		badguys.Add(new FirstOrder("General Hux",20));
+		badguys.Add(new FirstOrder("Kylo Ren",400));
+		badguys.Add(new FirstOrder("Captian Phasma",150));
+
+		badguys.Sort();
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
